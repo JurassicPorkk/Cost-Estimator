05a09a48abf69adc11eace9d01c9a4a3d6b94476
JurassicPorkk <dustinsteele383@gmail.com>
2025-05-17 03:44:18 +0000

    Update tab title to Closing Clerk

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import obfuscator from 'rollup-plugin-obfuscator';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    obfuscator({
+      compact: true,
+      controlFlowFlattening: true,
+      deadCodeInjection: true,
+      debugProtection: true,
+      disableConsoleOutput: true,
+    }),
+  ],
+  build: {
+    sourcemap: false, // Don't expose your source maps
+  },
+});
